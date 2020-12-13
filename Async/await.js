14284b5f14f5908f1d3b419ee42242fe7a360ab3
Andy Chen <chenyangpoppin@hotmail.com>
2020-12-13 18:13:31 +0800

    feat: add a simple await example

--- Async/await.js
+++ Async/await.js
@@ -16,3 +16,28 @@ hello().then(
     console.log(value); // hello
   }
 );
+
+const imageUrl = 'http://www.tjcchen.cn/love.png';
+
+// fetch url data
+const myFetch = async () => {
+  let response = await fetch(imageUrl, { mode: 'no-cors' });
+  if (!response.ok) {
+    throw new Error(`Http error! status: ${response.status}`);
+  } else {
+    return await response.blob();
+  }
+};
+
+myFetch().then(
+  (blob) => {
+    let objectUrl = URL.createObjectURL(blob);
+    let image = document.createElement('img');
+    image.src = objectUrl;
+    document.body.appendChild(image);
+  }
+).catch(
+  (e) => {
+    console.log(e);
+  }
+);
